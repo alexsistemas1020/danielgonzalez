bd86ad5b408faa0305f73ae4b6b6d8c5e4baa638
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2009-08-13 13:41:14 +0000

    ignoring tmp and log adding default value feature to form inputs

--- trunk/public/javascripts/application.js
+++ trunk/public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function clickclear(thisfield, defaultvalue) {
+    if (thisfield.value == defaultvalue) {
+        thisfield.value = "";
+    }
+}
+
+function clickrecall(thisfield, defaultvalue) {
+    if (thisfield.value == "") {
+        thisfield.value = defaultvalue;
+    }
+}
