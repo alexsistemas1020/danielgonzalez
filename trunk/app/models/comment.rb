c21471581e541c5111d1d0bbbc2b370ed8ce4f18
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2009-08-18 01:19:18 +0000

    deleting comments counter on posts improving post's show view removing innecesary partial for comments form

--- trunk/app/models/comment.rb
+++ trunk/app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post, :counter_cache => true, :conditions => ['status = ?', 'approved']
+  belongs_to :post
 
   validates_presence_of :content, :author, :author_email
   validates_length_of :content, :maximum => 1000
--- trunk/db/migrate/20090810001145_create_posts.rb
+++ trunk/db/migrate/20090810001145_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.integer :comments_count, :default => 0
       t.timestamps
     end
   end
