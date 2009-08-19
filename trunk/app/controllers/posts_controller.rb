71fce091d3e2ce1ebe4a711ce3e068f75d88c1cf
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2009-08-19 14:48:49 +0000

    adding rss feeds adding portfolio controller deleting edit, new and delete functions from posts

--- trunk/app/controllers/posts_controller.rb
+++ trunk/app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
 
-  
   def add_comment
 		@comment = Comment.new(params[:comment])
 		@post = Post.find(params[:id])
@@ -16,8 +15,10 @@ class PostsController < ApplicationController
 
   # GET /posts
   # GET /posts.xml
+  # GET /posts.rss
   # GET /posts/tagged_with/tag
   # GET /posts/tagged_with/tag.xml
+  # GET /posts/tagged_with/tag.rss
   def index
     @comment = Comment.new
     if params[:tag]
@@ -27,6 +28,7 @@ class PostsController < ApplicationController
     end
 
     respond_to do |format|
+      format.rss  { render :layout => false } # index.rss.erb
       format.html # index.html.erb
       format.xml  { render :xml => @posts }
     end
@@ -41,68 +43,6 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @post }
-    end
-  end
-
-  # GET /posts/new
-  # GET /posts/new.xml
-  def new
-    @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @post }
-    end
-  end
-
-  # GET /posts/1/edit
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  # POST /posts
-  # POST /posts.xml
-  def create
-    @post = Post.new(params[:post])
-
-    respond_to do |format|
-      if @post.save
-        flash[:notice] = 'Post was successfully created.'
-        format.html { redirect_to(@post) }
-        format.xml  { render :xml => @post, :status => :created, :location => @post }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /posts/1
-  # PUT /posts/1.xml
-  def update
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        flash[:notice] = 'Post was successfully updated.'
-        format.html { redirect_to(@post) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.xml
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(posts_url) }
-      format.xml  { head :ok }
     end
   end
 end
