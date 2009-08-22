c94f869ef6952a2733c352462eab3527aea557a1
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2009-08-22 16:35:41 +0000

    retouching favicon configuring pagination on post's index changing details on templates and layout

--- trunk/app/controllers/contacts_controller.rb
+++ trunk/app/controllers/contacts_controller.rb
@@ -10,11 +10,11 @@ class ContactsController < ApplicationController
     respond_to do |format|
       if @contact.save
         flash[:notice] = 'Message successfully sent.'
-        format.html { redirect_to(new_contacts_url) }
+        format.html { redirect_to(new_contact_url) }
         format.xml  { render :xml => @contact, :status => :created, :location => @contact }
       else
-        # flash[:notice] = 'Your Message could not be sent.'
-        format.html { render :action => "new" }
+        flash[:notice] = 'Your Message could not be sent.'
+        format.html { render :action => "new", :contact => @contact }
         format.xml  { render :xml => @contact.errors, :status => :unprocessable_entity }
       end
     end
