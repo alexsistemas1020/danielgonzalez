8f221803dc317edb2a0701050677ccb3e3be303e
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2009-08-23 19:13:04 +0000

    adding recaptcha plugin adapting contact form & mailer

--- trunk/app/models/comment.rb
+++ trunk/app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
 
-  validates_presence_of :content, :author, :author_email
+  validates_presence_of :content, :author_name, :author_email
   validates_length_of :content, :maximum => 1000
   validates_format_of :author_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  # validates_format_of :author_url, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
-  validates_exclusion_of :author, :in => 'Your Name...'
+  validates_format_of :author_url, :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+  validates_exclusion_of :author_name, :in => 'Your Name...'
   validates_exclusion_of :author_email, :in => 'Your Email...'
   validates_exclusion_of :content, :in => 'Your Message...'
   validates_inclusion_of :status, :in => %w(new approved spam)
