9c9c2d58abffb9c03e93f6892606f4b3d9f30bd1
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2010-02-09 00:22:38 +0000

    eliminando plugin innecesario adaptando a rails 2.3.5

--- trunk/config/environments/production.rb
+++ trunk/config/environments/production.rb
@@ -23,12 +23,6 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
-config.action_mailer.perform_deliveries = false
-config.action_mailer.delivery_method    = :sendmail
-config.action_mailer.sendmail_settings  = {
-  :location => '/usr/sbin/sendmail',
-  :arguments => '-i -t'
-}
 
 # Enable threaded mode
 # config.threadsafe!
