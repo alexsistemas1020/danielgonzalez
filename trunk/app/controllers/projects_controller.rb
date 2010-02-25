7c58f4d14136e4a7d161675ad7b08f8bdf535674
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2010-02-25 22:55:39 +0000

    agregando paginacion con AJAX

--- trunk/app/controllers/projects_controller.rb
+++ trunk/app/controllers/projects_controller.rb
@@ -3,11 +3,10 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
+    @projects = Project.paginate(:page => params[:page], :per_page => 2, :order => 'created_at DESC')
     respond_to do |format|
-      format.html do # index.html.erb
-        @projects = Project.paginate(:page => params[:page], :per_page => 2, :order => 'created_at DESC')
-      end
-      @projects = Project.all(:order => 'created_at DESC')
+      format.html { @projects }
+      format.js   { render :partial => 'projects' }
       format.xml  { render :xml => @projects }
     end
   end
