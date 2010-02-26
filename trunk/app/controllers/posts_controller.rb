dc532aa66e16866a4a550ad18230192a0a5931e1
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2010-02-26 13:11:54 +0000

    quitando paginacion en posts

--- trunk/app/controllers/posts_controller.rb
+++ trunk/app/controllers/posts_controller.rb
@@ -21,16 +21,14 @@ class PostsController < ApplicationController
   # GET /posts/tagged_with/tag.rss
   def index
     @comment = Comment.new
+    @posts = Post.all(:order => 'created_at DESC')
     
     respond_to do |format|
       format.html do # index.html.erb
         if params[:tag]
-          @posts = Post.tagged_with(params[:tag], :on => :tags, :order => 'created_at DESC')
-        else
-          @posts = Post.paginate(:page => params[:page], :per_page => 2, :order => 'created_at DESC')
+          @posts = Post.find_tagged_with(params[:tag], :order => "created_at DESC")
         end
       end
-      @posts = Post.all(:order => 'created_at DESC')
       format.rss  { render :layout => false } # index.rss.erb
       format.xml  { render :xml => @posts }
     end
