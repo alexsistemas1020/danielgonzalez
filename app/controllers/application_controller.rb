8fbda9d7220430cf01791dab91c89a21d2c38a62
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2010-03-09 02:07:21 +0000

    revirtiendo feature para twitter acomodando tag para usar cammelcase y evitar error en servidor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require 'rubygems'
-require 'twitter'
 
 class ApplicationController < ActionController::Base
-  before_filter :get_twitter_timeline
+  #  before_filter :get_twitter_timeline
 
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
@@ -12,8 +10,8 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
-  def get_twitter_timeline
-    twitter = Twitter::Client.new(:login => 'dangt85', :password => 'm1cl4v3Ss')
-    @my_timeline = twitter.timeline_for(:me, :count => 5)
-  end
+  #  def get_twitter_timeline
+  #    twitter = Twitter::Client.new(:login => 'dangt85', :password => 'm1cl4v3Ss')
+  #    @my_timeline = twitter.timeline_for(:me, :count => 5)
+  #  end
 end
