2cbd888724cc19ada1503b292c88c53fe28d240b
dangt85 <dangt85@85826f9c-8806-11de-8f56-7b2e0289298d>
2010-03-16 16:40:12 +0000

    eliminando twitter feature; agregando proyecto rfid4r; agregando parametro target _blank en links

--- trunk/app/controllers/application_controller.rb
+++ trunk/app/controllers/application_controller.rb
@@ -2,16 +2,9 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  #  before_filter :get_twitter_timeline
-
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-
-  #  def get_twitter_timeline
-  #    twitter = Twitter::Client.new(:login => 'dangt85', :password => 'm1cl4v3Ss')
-  #    @my_timeline = twitter.timeline_for(:me, :count => 5)
-  #  end
 end
